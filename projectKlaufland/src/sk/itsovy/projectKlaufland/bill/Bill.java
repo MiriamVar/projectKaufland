f741560c4b0d834ba707a5c5bd1d19bf13bca11b
Miriam Vargockova <mimika648@gmail.com>
2019-03-28 09:14:22 +0100

    adding items to the database

---
+++ projectKlaufland/src/sk/itsovy/projectKlaufland/bill/Bill.java
@@ -0,0 +1,115 @@
+package sk.itsovy.projectKlaufland.bill;
+
+import sk.itsovy.projectKlaufland.Exepction.BillException;
+import sk.itsovy.projectKlaufland.database.Database;
+import sk.itsovy.projectKlaufland.items.Item;
+
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import sk.itsovy.projectKlaufland.items.Piece;
+import sk.itsovy.projectKlaufland.items.drink.DraftInterface;
+import sk.itsovy.projectKlaufland.items.food.Fruit;
+import sk.itsovy.projectKlaufland.main.Globals;
+
+public class Bill {
+    private List<Item> list;
+    private int count;
+    private  double sum;
+    private boolean open;
+    private  Date date;
+
+    public Bill() {
+        this.list = new ArrayList<>();
+        count = 0;
+        open=true;
+    }
+
+    public List<Item> getList() {
+        return list;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void end() throws SQLException {
+        if(open){
+            date = new Date();
+            System.out.println( date);
+            Database db = Database.getInstanceDB();
+            db.insertNewBill(this);
+
+        }
+        else {
+            System.out.println("neni uzavreny");
+        }
+
+        open=false;
+
+    }
+
+    public void addItem(Item item) throws BillException {
+        if (item != null) {
+            if (!open){
+                String message = "Bill is closed. Is not allowed to add new items";
+                throw new BillException(message);
+            }
+            if (count == Globals.MAXTERMS) {
+                String message = "Bill is fulll, max is " + Globals.MAXTERMS + " items.";
+                throw new BillException(message);
+            }
+            list.add(item);
+            count++;
+
+        }
+    }
+
+    public void removeItem(Item item) {
+        if (list.contains(item)){
+            list.remove(item);
+            count--;
+        }
+    }
+
+    public double getFinalPrice(){
+        sum=0;
+        for(Item item: list){
+            sum+=item.getTotalPrice();
+        }
+        System.out.println("Final price: ");
+        return sum;
+//        throw new UnsupportedOperationException("Method does not exists yet");
+    }
+
+    public int getCount(){
+        return count;
+    }
+
+    public void print(){
+        if (count==0){
+            System.out.println("Nothing to print. bill is empty");
+        } else{
+            for (Item item:list) {
+                if(item instanceof DraftInterface) {
+                    System.out.println("Name: "+item.getName() + " Count " + ((DraftInterface) item).getVolume()+" Price: "+item.getPrice() + " TotalPrice: " + item.getTotalPrice());
+                }
+                else if(item instanceof Fruit){
+                    System.out.println("Name: "+item.getName() + " Count " + ((Fruit) item).getWeight()+" Price: "+item.getPrice() + " TotalPrice: " + item.getTotalPrice());
+                }
+                else if(item instanceof Piece){
+                    System.out.println("Name: "+item.getName() + " Count " + ((Piece) item).getAmount() +" Price: "+item.getPrice() + " TotalPrice: " + item.getTotalPrice());
+                }
+            }
+        }
+    }
+
+//    public double getTotalPriceUSD(){
+//        double totalPrice = getFinalPrice();
+//
+//    }
+
+}
--- projectKlaufland/src/sk/itsovy/projectKlaufland/database/Database.java
+++ projectKlaufland/src/sk/itsovy/projectKlaufland/database/Database.java
@@ -1,6 +1,10 @@
 package sk.itsovy.projectKlaufland.database;
 
 import sk.itsovy.projectKlaufland.bill.Bill;
+import sk.itsovy.projectKlaufland.items.Item;
+import sk.itsovy.projectKlaufland.items.Piece;
+import sk.itsovy.projectKlaufland.items.drink.DraftInterface;
+import sk.itsovy.projectKlaufland.items.food.Fruit;
 
 import java.sql.*;
 
@@ -38,18 +42,55 @@ public class Database {
     public void insertNewBill(Bill bill) throws SQLException {
         Connection con = getConnection();
         String query = "insert into bill(totalPrice,date, time) values(?,?,?)";
-        try{
+        String query2 = "insert into item(orderId,name,price,count,unit) values(?,?,?,?,?)";
+        try {
             con.setAutoCommit(false);
             PreparedStatement statement = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
-            statement.setDouble(1,bill.getFinalPrice());
-            statement.setDate(2,new java.sql.Date(bill.getDate().getTime()));
-            statement.setTime(3,new java.sql.Time(bill.getDate().getTime()));
+            statement.setDouble(1, bill.getFinalPrice());
+            statement.setDate(2, new java.sql.Date(bill.getDate().getTime()));
+            statement.setTime(3, new java.sql.Time(bill.getDate().getTime()));
 
-            statement.executeUpdate();
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) {
+                throw new SQLException("Creating user failed, no rows affected.");
+            }
+            try (ResultSet generatedKeys = statement.getGeneratedKeys()) {
+//                while (generatedKeys.next()) {
+//                    System.out.println(generatedKeys.getLong(1));
+////                    user.setId(generatedKeys.getLong(1));
+//                }
+                if (generatedKeys.next()){
+                    for (Item item : bill.getList()) {
+                        PreparedStatement statement2 = con.prepareStatement(query2, Statement.RETURN_GENERATED_KEYS);
+                        statement2.setDouble(1, generatedKeys.getLong(1));
+                        statement2.setString(2, item.getName());
+                        statement2.setDouble(3, item.getPrice());
+
+                        if (item instanceof DraftInterface) {
+                            statement2.setDouble(4, ((DraftInterface) item).getVolume());
+                            statement2.setString(5, "l");
+                        } else if (item instanceof Fruit) {
+                            statement2.setDouble(4, ((Fruit) item).getWeight());
+                            statement2.setString(5, "kg");
+                        } else if (item instanceof Piece) {
+                            statement2.setDouble(4, ((Piece) item).getAmount());
+                            statement2.setString(5, "pcs");
+                        }
+
+                        statement2.executeUpdate();
+
+                    }
+                }
+                else {
+                    throw new SQLException("Creating bill failed, no ID obtained.");
+                }
+
+            }
             con.commit();
             con.close();
-
-        }  catch (SQLException e) {
+        }
+            catch (SQLException e) {
             e.printStackTrace();
             con.rollback();
         }
